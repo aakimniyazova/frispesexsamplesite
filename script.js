9c3546fe613eadc341adf327bacaff2b8c9cde02
aakimniyazova <118665301+aakimniyazova@users.noreply.github.com>
2022-11-21 12:40:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function change1() {
+    document.getElementById('pic').src='./img/Spacious parking area.jpg';
+}
+
+function change2() {
+    document.getElementById('pic').src='./img/Comfortable spaces.jpg';
+}
+
+function change3() {
+    document.getElementById('pic').src='./img/Cozy cafe.png';
+}
+
+function change4() {
+    document.getElementById('pic').src='./img/Child playground.jpg';
+}
+
+function change5() {
+    document.getElementById('pic').src='./img/Outdoor spaces.jfif';
+}
+
